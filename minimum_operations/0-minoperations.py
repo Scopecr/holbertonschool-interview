7ec51dec6d963c570a45e1ff5c200a9e6dab83e1
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-01-25 15:50:32 -0400

    feat: refactor minOperations function for improved logic and add docstring

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def minOperations(n):
+    """Minimun operations
+    Returns and integer
+    id n is impossible to achieve return 0
+    """
     if n <= 1:
         return 0
-        
-    operations = 0
-    d = 2
-    
-    while n > 1:
-        while n % d == 0:
-            operations += d  # d operations: 1 copy + (d-1) pastes
-            n //= d
-        d += 1
-        
-    return operations
-  
+    for i in range(2, n +1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return 0
