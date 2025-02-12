4a146754b32fe820967b6abd370c9c84946f415d
Scopecr <wjrcarrion@gmail.com>
2025-02-12 18:23:23 -0500

    docs: add docstring to log parsing script for clarity on functionality

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Script that reads stdin line by line and computes metrics:
+- Input format: '<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>'
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print statistics
+- Total file size: File size: <total size>
+- Number of lines by status code in ascending order
+"""
 import sys
 import signal
 import re
