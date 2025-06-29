15ed648e63a47cef640e21d01f71e8812127659f
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-06-28 22:24:27 -0400

    Refactor node creation and update function prototypes in 0-add_node.c

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -1,110 +1,90 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
 
-/* Function prototypes in case they're missing from list.h */
-List *add_node_end(List **list, char *str);
-List *add_node_begin(List **list, char *str);
-
 /**
- * add_node_end - Add a new node to the end of a double circular linked list
- * @list: Pointer to the list to modify
- * @str: String to copy into the new node
+ * create_node - Helper function to create a new node with a copy of str
+ * @str: The string to copy
  *
- * Return: Address of the new node, or NULL on failure
+ * Return: Pointer to the new node or NULL on failure
  */
-List *add_node_end(List **list, char *str)
+static List *create_node(char *str)
 {
-		List *new_node;
-		List *last;
-
-		if (!list || !str)
-				return (NULL);
-
-		/* Create new node */
-		new_node = malloc(sizeof(List));
+		List *new_node = malloc(sizeof(List));
 		if (!new_node)
-				return (NULL);
+				return NULL;
 
-		/* Copy string */
-		new_node->str = malloc(strlen(str) + 1);
+		new_node->str = strdup(str);
 		if (!new_node->str)
 		{
 				free(new_node);
-				return (NULL);
-		}
-		strcpy(new_node->str, str);
-
-		/* If list is empty */
-		if (!*list)
-		{
-				new_node->next = new_node;
-				new_node->prev = new_node;
-				*list = new_node;
-				return (new_node);
+				return NULL;
 		}
 
-		/* Find the last node (the one before the head) */
-		last = (*list)->prev;
-
-		/* Insert new node at the end */
-		new_node->next = *list;
-		new_node->prev = last;
-		last->next = new_node;
-		(*list)->prev = new_node;
-
-		return (new_node);
+		new_node->next = new_node->prev = NULL;
+		return new_node;
 }
 
 /**
- * add_node_begin - Add a new node to the beginning of a double circular linked list
- * @list: Pointer to the list to modify
- * @str: String to copy into the new node
+ * add_node_begin - Adds a new node to the beginning of a double circular list
+ * @list: Pointer to the list head pointer
+ * @str: The string to duplicate into the new node
  *
  * Return: Address of the new node, or NULL on failure
  */
 List *add_node_begin(List **list, char *str)
 {
-		List *new_node;
-		List *last;
-
-		if (!list || !str)
-				return (NULL);
-
-		/* Create new node */
-		new_node = malloc(sizeof(List));
+		List *new_node = create_node(str);
 		if (!new_node)
-				return (NULL);
+				return NULL;
 
-		/* Copy string */
-		new_node->str = malloc(strlen(str) + 1);
-		if (!new_node->str)
+		if (!(*list))
 		{
-				free(new_node);
-				return (NULL);
-		}
-		strcpy(new_node->str, str);
-
-		/* If list is empty */
-		if (!*list)
-		{
-				new_node->next = new_node;
-				new_node->prev = new_node;
+				new_node->next = new_node->prev = new_node;
 				*list = new_node;
-				return (new_node);
+		}
+		else
+		{
+				List *last = (*list)->prev;
+
+				new_node->next = *list;
+				new_node->prev = last;
+				last->next = new_node;
+				(*list)->prev = new_node;
+				*list = new_node;
 		}
 
-		/* Find the last node (the one before the current head) */
-		last = (*list)->prev;
+		return new_node;
+}
 
-		/* Insert new node at the beginning */
-		new_node->next = *list;
-		new_node->prev = last;
-		(*list)->prev = new_node;
-		last->next = new_node;
+/**
+ * add_node_end - Adds a new node to the end of a double circular list
+ * @list: Pointer to the list head pointer
+ * @str: The string to duplicate into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+		List *new_node = create_node(str);
+		if (!new_node)
+				return NULL;
 
-		/* Update head pointer */
-		*list = new_node;
+		if (!(*list))
+		{
+				new_node->next = new_node->prev = new_node;
+				*list = new_node;
+		}
+		else
+		{
+				List *last = (*list)->prev;
 
-		return (new_node);
+				new_node->next = *list;
+				new_node->prev = last;
+				last->next = new_node;
+				(*list)->prev = new_node;
+		}
+
+		return new_node;
 }
