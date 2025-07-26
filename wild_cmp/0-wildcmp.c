59d537d315218685fcb5eaa2df239e7ddd721dff
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-07-26 13:54:33 -0400

    Fix indentation for improved readability in wildcmp function

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -11,18 +11,18 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0')
-			return (*s1 == '\0');
+		return (*s1 == '\0');
 
 	if (*s2 == '*')
 	{
-			if (wildcmp(s1, s2 + 1))  // '*' matches zero characters
-					return 1;
-			if (*s1 != '\0' && wildcmp(s1 + 1, s2))  // '*' matches one or more characters
-					return 1;
+		if (wildcmp(s1, s2 + 1))  // '*' matches zero characters
+			return 1;
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))  // '*' matches one or more characters
+				return 1;
 	}
 	else if (*s1 == *s2)
 	{
-			return wildcmp(s1 + 1, s2 + 1);
+		return wildcmp(s1 + 1, s2 + 1);
 	}
 
 	return 0;
