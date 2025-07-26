8105b40349e59467683dca5819d17f051a62415f
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-07-26 13:55:13 -0400

    Refactor comments in wildcmp function for clarity

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -15,9 +15,9 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		if (wildcmp(s1, s2 + 1))  // '*' matches zero characters
+		if (wildcmp(s1, s2 + 1))
 			return 1;
-		if (*s1 != '\0' && wildcmp(s1 + 1, s2))  // '*' matches one or more characters
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 				return 1;
 	}
 	else if (*s1 == *s2)
