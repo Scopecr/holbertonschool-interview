0e8322cefdb73ce67e7780999dd01d81a2ed5d70
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-08-03 14:44:41 -0400

    Update module docstring for pascal_triangle function

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines the pascal_triangle function that returns
+a list of lists representing Pascal’s Triangle.
+"""
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing the Pascal’s triangle of n
